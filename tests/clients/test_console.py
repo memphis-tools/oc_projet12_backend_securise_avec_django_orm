e140d2b1db60e6b07c74541404aed37add022c6a
memphis-tools <rn@github.com>
2023-07-28 15:27:50 +0200

    Adaptation tests suite évolution de la gestion authent/autorisations des comptes utilisateurs (utilisateurs deviennent des roles/users de postgresql)

--- tests/clients/test_console.py
+++ tests/clients/test_console.py
@@ -2,13 +2,23 @@
 Description:
 Test du client en mode console de l'application.
 """
+import pytest
+import jwt
 
 try:
+    from src.views.views import AppViews
     from src.commands import database_read_commands
     from src.commands import authentication_commands
+    from src.utils.utils import authentication_permission_decorator, display_banner
+    from src.clients.console import ConsoleClient
+    from src.views.jwt_view import JwtView
 except ModuleNotFoundError:
+    from views.views import AppViews
     from commands import database_read_commands
     from commands import authentication_commands
+    from utils.utils import authentication_permission_decorator, display_banner
+    from clients.console import ConsoleClient
+    from views.jwt_view import JwtView
 
 
 def test_get_clients(get_runner):
@@ -44,8 +54,3 @@ def test_get_locations(get_runner):
 def test_get_roles(get_runner):
     result = get_runner.invoke(database_read_commands.get_roles)
     assert result.exit_code == 0
-
-
-def test_get_token(get_runner):
-    result = get_runner.invoke(authentication_commands.get_token)
-    assert result.exit_code == 2
