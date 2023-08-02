f126a6146e969b0c365c687f2eb67987b3663cd0
memphis-tools <rn@github.com>
2023-08-02 02:20:09 +0200

    Permettre un pseudo mock de la bdd, on purge toutes données non utilisées. On utilise black pour mettre à jour divers modules du pack tests.

--- tests/views/test_z_purge.py
+++ tests/views/test_z_purge.py
@@ -4,13 +4,16 @@ Nous sommes dans un contexte de développement, sans bdd dédiée à l'environne
 On purge tout enregistrement crée pendant la phase de test.
 """
 import pytest
+
 try:
     from src.clients.delete_console import ConsoleClientForDelete
 except ModuleNotFoundError:
     from clients.delete_console import ConsoleClientForDelete
 
 
-def test_delete_client_view(get_runner, get_valid_decoded_token_for_a_commercial_collaborator):
+def test_delete_client_view(
+    get_runner, get_valid_decoded_token_for_a_commercial_collaborator
+):
     try:
         result = ConsoleClientForDelete().delete_client("dduck")
         assert isinstance(result, int)
@@ -19,7 +22,9 @@ def test_delete_client_view(get_runner, get_valid_decoded_token_for_a_commercial
         print(error)
 
 
-def test_delete_company_view(get_runner, get_valid_decoded_token_for_a_commercial_collaborator):
+def test_delete_company_view(
+    get_runner, get_valid_decoded_token_for_a_commercial_collaborator
+):
     try:
         result = ConsoleClientForDelete().delete_company("cal7778")
         assert isinstance(result, int)
@@ -28,7 +33,9 @@ def test_delete_company_view(get_runner, get_valid_decoded_token_for_a_commercia
         print(error)
 
 
-def test_delete_contract_view(get_runner, get_valid_decoded_token_for_a_commercial_collaborator):
+def test_delete_contract_view(
+    get_runner, get_valid_decoded_token_for_a_commercial_collaborator
+):
     try:
         result = ConsoleClientForDelete().delete_contract("D9Z1")
         assert isinstance(result, int)
@@ -37,7 +44,9 @@ def test_delete_contract_view(get_runner, get_valid_decoded_token_for_a_commerci
         print(error)
 
 
-def test_delete_event_view(get_runner, get_valid_decoded_token_for_a_commercial_collaborator):
+def test_delete_event_view(
+    get_runner, get_valid_decoded_token_for_a_commercial_collaborator
+):
     try:
         result = ConsoleClientForDelete().delete_event("FW971")
         assert isinstance(result, int)
@@ -46,7 +55,9 @@ def test_delete_event_view(get_runner, get_valid_decoded_token_for_a_commercial_
         print(error)
 
 
-def test_delete_location_view(get_runner, get_valid_decoded_token_for_a_commercial_collaborator):
+def test_delete_location_view(
+    get_runner, get_valid_decoded_token_for_a_commercial_collaborator
+):
     try:
         result = ConsoleClientForDelete().delete_location("CAL13540")
         assert isinstance(result, int)
@@ -55,7 +66,9 @@ def test_delete_location_view(get_runner, get_valid_decoded_token_for_a_commerci
         print(error)
 
 
-def test_delete_role_view(get_runner, get_valid_decoded_token_for_a_gestion_collaborator):
+def test_delete_role_view(
+    get_runner, get_valid_decoded_token_for_a_gestion_collaborator
+):
     try:
         result = ConsoleClientForDelete().delete_role("driv")
         assert isinstance(result, int)
@@ -64,7 +77,9 @@ def test_delete_role_view(get_runner, get_valid_decoded_token_for_a_gestion_coll
         print(error)
 
 
-def test_delete_department_view(get_runner, get_valid_decoded_token_for_a_gestion_collaborator):
+def test_delete_department_view(
+    get_runner, get_valid_decoded_token_for_a_gestion_collaborator
+):
     try:
         result = ConsoleClientForDelete().delete_department("logist")
         assert isinstance(result, int)
@@ -74,7 +89,9 @@ def test_delete_department_view(get_runner, get_valid_decoded_token_for_a_gestio
 
 
 @pytest.mark.parametrize("matricule", ["pp123456789", "qq123456789", "rr123456789"])
-def test_delete_collaborator_view(get_runner, get_valid_decoded_token_for_a_gestion_collaborator, matricule):
+def test_delete_collaborator_view(
+    get_runner, get_valid_decoded_token_for_a_gestion_collaborator, matricule
+):
     try:
         result = ConsoleClientForDelete().delete_collaborator(matricule)
         assert isinstance(result, int)
